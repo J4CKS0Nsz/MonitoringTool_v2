47de89d999d3014d7e0b0c97c9a252c4c7731a5e
Jackson <None>
2023-03-26 23:43:51 +0200

    - Ping Überprüfung: Konfigparameter miteinbezogen, Ping-Funktion umdefiniert, Netzwerkausfall/Ping-Fehler miteinbezogen - Importierung der Bibliotheken auf das nötigste reduziert

--- MonitoringTool_v2/MonitoringTool_v2.py
+++ MonitoringTool_v2/MonitoringTool_v2.py
@@ -1,13 +1,13 @@
 from datetime import datetime
-import configparser
-import smtplib
-import time
-from icmplib import ping
-import psutil
+from time import sleep
+from configparser import ConfigParser as configparser
+from smtplib import SMTP_SSL as smtp_ssl
+from psutil import cpu_percent, virtual_memory, disk_usage
+from icmplib import ping, exceptions as pingexc
 
 
 ##READCONFIG
-conf = configparser.ConfigParser()
+conf = configparser()
 conf.read("MonitoringTool.ini")
 conf_mail_server = conf.get("alertmail", "server")
 conf_mail_sslport = conf.getint("alertmail", "sslport")
@@ -20,6 +20,8 @@ conf_ram_warn_maxmb = conf.getint("ram", "warn_maxmb")
 conf_disk_path = conf.get("disk", "checkpath")
 conf_disk_warn_maxpct = conf.getfloat("disk", "warn_maxpct")
 conf_disk_warn_maxgb = conf.getint("disk", "warn_maxgb")
+conf_network_ping_address = conf.get("network", "ping_address")
+conf_network_warn_maxping = conf.getfloat("network", "warn_maxping")
 
 
 ##LOG
@@ -38,7 +40,7 @@ Subject: {}
 
 {}""".format(conf_mail_from,conf_mail_to,conf_mail_to,mail_subjekt,mail_message)
     print(mail_content)
-    mail_server = smtplib.SMTP_SSL(host=conf_mail_server,port=conf_mail_sslport)
+    mail_server = smtp_ssl(host=conf_mail_server,port=conf_mail_sslport)
     mail_server.login(conf_mail_from,conf_mail_pass)
     mail_server.sendmail(conf_mail_from,conf_mail_to,mail_content)
     mail_server.quit()
@@ -53,113 +55,137 @@ def get_current_date():
 
 ##CPU
 def get_current_cpu_usage_pct():
-    return psutil.cpu_percent(interval=0.1)
+    return cpu_percent(interval=0.1)
 
 ##RAM
 def get_current_ram_usage_pct():
-    return psutil.virtual_memory().percent
+    return virtual_memory().percent
 
 def get_current_ram_usage():
-    return int(float(psutil.virtual_memory().used)/1024/1024)
+    return int(float(virtual_memory().used)/1024/1024)
 
 def get_ram_total():
-    return int(float(psutil.virtual_memory().total)/1024/1024)
+    return int(float(virtual_memory().total)/1024/1024)
 
 ##DISK
 def get_current_disk_usage_pct():
-    return psutil.disk_usage(conf_disk_path).percent
+    return disk_usage(conf_disk_path).percent
 
 def get_current_disk_usage():
-    return int(float(psutil.disk_usage(conf_disk_path).used)/1024/1024/1024)
+    return int(float(disk_usage(conf_disk_path).used)/1024/1024/1024)
 
 def get_disk_total():
-    return int(float(psutil.disk_usage(conf_disk_path).total)/1024/1024/1024)
+    return int(float(disk_usage(conf_disk_path).total)/1024/1024/1024)
 
 ##Network
-def get_ping():
-    return ping("google.com", count=4, interval=1, timeout=1).avg_rtt
+def get_current_ping():
+    try:
+        current_ping = ping(conf_network_ping_address, count=4, interval=1, timeout=1).avg_rtt
+    except pingexc.NameLookupError:
+        return 0
+    else:
+        return current_ping
+
 
 #do_report("hello","Warnung")
 while True:
     do_log("-------------------------------------------------------------------------------------")
 
-    cpu_usage_pct = get_current_cpu_usage_pct()
-    if cpu_usage_pct >= conf_cpu_warn_maxpct:
+
+    get_cpu_usage_pct = get_current_cpu_usage_pct()
+    if get_cpu_usage_pct >= conf_cpu_warn_maxpct:
         incident_start = get_current_datetime()
         do_log(str(get_current_datetime())+" :: <WARNUNG>    : CPU              : Auslastung ueber "+str(conf_cpu_warn_maxpct)+" %")
-        while cpu_usage_pct >= conf_cpu_warn_maxpct:
-            do_log(str(get_current_datetime())+" :: <WARNUNG>    : CPU              : Auslastung: "+str(cpu_usage_pct)+" %")
-            time.sleep(1)
-            cpu_usage_pct = get_current_cpu_usage_pct()
+        while get_cpu_usage_pct >= conf_cpu_warn_maxpct:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : CPU              : Auslastung: "+str(get_cpu_usage_pct)+" %")
+            sleep(1)
+            get_cpu_usage_pct = get_current_cpu_usage_pct()
         incident_end = get_current_datetime()
         incident_duration = incident_end-incident_start
         do_log(str(get_current_datetime())+" :: <ENTWARNUNG> : CPU              : Auslastung unter "+str(conf_cpu_warn_maxpct)+" % (Vorfalldauer: "+str(incident_duration)+")")
     else:
-        do_log(str(get_current_datetime())+" :: <OK>         : CPU              : Auslastung: "+str(cpu_usage_pct)+" %")
+        do_log(str(get_current_datetime())+" :: <OK>         : CPU              : Auslastung: "+str(get_cpu_usage_pct)+" %")
 
 
-    ram_usage_pct = get_current_ram_usage_pct()
-    if ram_usage_pct >= conf_ram_warn_maxpct:
+    get_ram_usage_pct = get_current_ram_usage_pct()
+    if get_ram_usage_pct >= conf_ram_warn_maxpct:
         incident_start = get_current_datetime()
         do_log(str(get_current_datetime())+" :: <WARNUNG>    : RAM              : Ueber "+str(conf_ram_warn_maxpct)+" % belegt")
-        while ram_usage_pct >= conf_ram_warn_maxpct:
-            do_log(str(get_current_datetime())+" :: <WARNUNG>    : RAM              : In Verwendung: "+str(ram_usage_pct)+" %")
-            time.sleep(1)
-            ram_usage_pct = get_current_ram_usage_pct()
+        while get_ram_usage_pct >= conf_ram_warn_maxpct:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : RAM              : In Verwendung: "+str(get_ram_usage_pct)+" %")
+            sleep(1)
+            get_ram_usage_pct = get_current_ram_usage_pct()
         incident_end = get_current_datetime()
         incident_duration = incident_end-incident_start
         do_log(str(get_current_datetime())+" :: <ENTWARNUNG> : RAM              : Unter "+str(conf_ram_warn_maxpct)+" % belegt (Vorfalldauer: "+str(incident_duration)+")")
     else:
-        do_log(str(get_current_datetime())+" :: <OK>         : RAM              : In Verwendung: "+str(ram_usage_pct)+" %")
+        do_log(str(get_current_datetime())+" :: <OK>         : RAM              : In Verwendung: "+str(get_ram_usage_pct)+" %")
 
 
-    ram_usage = get_current_ram_usage()
-    if ram_usage >= conf_ram_warn_maxmb:
+    get_ram_usage = get_current_ram_usage()
+    if get_ram_usage >= conf_ram_warn_maxmb:
         incident_start = get_current_datetime()
         do_log(str(get_current_datetime())+" :: <WARNUNG>    : RAM              : Ueber "+str(conf_ram_warn_maxmb)+" MB belegt")
-        while ram_usage >= conf_ram_warn_maxmb:
-            do_log(str(get_current_datetime())+" :: <WARNUNG>    : RAM              : In Verwendung: "+str(ram_usage)+" MB von "+str(get_ram_total())+" MB")
-            time.sleep(1)
-            ram_usage = get_current_ram_usage()
+        while get_ram_usage >= conf_ram_warn_maxmb:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : RAM              : In Verwendung: "+str(get_ram_usage)+" MB von "+str(get_ram_total())+" MB")
+            sleep(1)
+            get_ram_usage = get_current_ram_usage()
         incident_end = get_current_datetime()
         incident_duration = incident_end-incident_start
         do_log(str(get_current_datetime())+" :: <ENTWARNUNG> : RAM              : Unter "+str(conf_ram_warn_maxmb)+" MB belegt (Vorfalldauer: "+str(incident_duration)+")")
     else:
-        do_log(str(get_current_datetime())+" :: <OK>         : RAM              : In Verwendung: "+str(ram_usage)+" MB von "+str(get_ram_total())+" MB")
+        do_log(str(get_current_datetime())+" :: <OK>         : RAM              : In Verwendung: "+str(get_ram_usage)+" MB von "+str(get_ram_total())+" MB")
 
 
-    disk_usage_pct = get_current_disk_usage_pct()
-    if disk_usage_pct >= conf_disk_warn_maxpct:
+    get_disk_usage_pct = get_current_disk_usage_pct()
+    if get_disk_usage_pct >= conf_disk_warn_maxpct:
         incident_start = get_current_datetime()
         do_log(str(get_current_datetime())+" :: <WARNUNG>    : Datentraeger "+conf_disk_path+" : Ueber "+str(conf_disk_warn_maxpct)+" % belegt")
-        while disk_usage_pct >= conf_disk_warn_maxpct:
-            do_log(str(get_current_datetime())+" :: <WARNUNG>    : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(disk_usage_pct)+" %")
-            time.sleep(1)
-            disk_usage_pct = get_current_disk_usage_pct()
+        while get_disk_usage_pct >= conf_disk_warn_maxpct:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(get_disk_usage_pct)+" %")
+            sleep(1)
+            get_disk_usage_pct = get_current_disk_usage_pct()
         incident_end = get_current_datetime()
         incident_duration = incident_end-incident_start
         do_log(str(get_current_datetime())+" :: <ENTWARNUNG> : Datentraeger "+conf_disk_path+" : Unter "+str(conf_disk_warn_maxpct)+" % belegt (Vorfalldauer: "+str(incident_duration)+")")
     else:
-        do_log(str(get_current_datetime())+" :: <OK>         : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(disk_usage_pct)+" %")
+        do_log(str(get_current_datetime())+" :: <OK>         : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(get_disk_usage_pct)+" %")
 
 
-    disk_usage = get_current_disk_usage()
-    if disk_usage >= conf_disk_warn_maxgb:
+    get_disk_usage = get_current_disk_usage()
+    if get_disk_usage >= conf_disk_warn_maxgb:
         incident_start = get_current_datetime()
         do_log(str(get_current_datetime())+" :: <WARNUNG>    : Datentraeger "+conf_disk_path+" : Ueber "+str(conf_disk_warn_maxgb)+" GB belegt")
-        while disk_usage >= conf_disk_warn_maxgb:
-            do_log(str(get_current_datetime())+" :: <WARNUNG>    : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(disk_usage)+" GB von "+str(get_disk_total())+" GB")
-            time.sleep(1)
-            disk_usage = get_current_disk_usage()
+        while get_disk_usage >= conf_disk_warn_maxgb:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(get_disk_usage)+" GB von "+str(get_disk_total())+" GB")
+            sleep(1)
+            get_disk_usage = get_current_disk_usage()
         incident_end = get_current_datetime()
         incident_duration = incident_end-incident_start
         do_log(str(get_current_datetime())+" :: <ENTWARNUNG> : Datentraeger "+conf_disk_path+" : Unter "+str(conf_disk_warn_maxgb)+" % belegt (Vorfalldauer: "+str(incident_duration)+")")
     else:
-        do_log(str(get_current_datetime())+" :: <OK>         : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(disk_usage)+" GB von "+str(get_disk_total())+" GB")
+        do_log(str(get_current_datetime())+" :: <OK>         : Datentraeger "+conf_disk_path+" : In Verwendung: "+str(get_disk_usage)+" GB von "+str(get_disk_total())+" GB")
+
+        
+    get_ping = get_current_ping()
+    if get_ping >= conf_network_warn_maxping or get_ping == 0:
+        incident_start = get_current_datetime()
+        if get_ping >= conf_network_warn_maxping:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : Netzwerk         : Ping ueber "+str(conf_network_warn_maxping)+" ms")
+        else:
+            do_log(str(get_current_datetime())+" :: <WARNUNG>    : Netzwerk         : Ping nicht moeglich")
+        while get_ping >= conf_network_warn_maxping or get_ping == 0:
+            if get_ping >= conf_network_warn_maxping:
+                do_log(str(get_current_datetime())+" :: <WARNUNG>    : Netzwerk         : Ping: "+str(get_ping)+" ms")
+            else:
+                do_log(str(get_current_datetime())+" :: <WARNUNG>    : Netzwerk         : Ping nicht moeglich")
+            sleep(1)
+            get_ping = get_current_ping()
+        incident_end = get_current_datetime()
+        incident_duration = incident_end-incident_start
+        do_log(str(get_current_datetime())+" :: <ENTWARNUNG> : Netzwerk         : Ping unter "+str(conf_network_warn_maxping)+" ms (Vorfalldauer: "+str(incident_duration)+")")
+    else:
+        do_log(str(get_current_datetime())+" :: <OK>         : Netzwerk         : Ping: "+str(get_ping)+" ms")
 
 
-    do_log(str(get_current_datetime())+" :: <OK>         : Netzwerk         : Ping: "+str(get_ping())+" ms")
-
-
-    time.sleep(2)
-exit
+    sleep(2)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MonitoringTool_v2
+
+Anforderungen: psutil, icmplib
+
+Alle Funktionen getestet in Windows 11 und Debian 11
