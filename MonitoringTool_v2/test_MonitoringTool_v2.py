8c592ad7f97bd4b1082b49083b5e32da91139ac0
aqri1 <128977110+aqri1@users.noreply.github.com>
2023-03-29 08:25:17 +0200

    Create test_MonitoringTool_v2.py
    Basic Unit tests für Monitoring Tool

---
+++ MonitoringTool_v2/test_MonitoringTool_v2.py
@@ -0,0 +1,15 @@
+import unittest
+from MonitoringTool import *
+
+def test_cpu_usage():
+    assert get_current_cpu_usage_pct() >= 0
+    assert get_current_cpu_usage_pct() <= 100
+    
+def test_ram_usage():
+    assert get_current_cpu_usage_pct() >= 0
+    assert get_current_cpu_usage_pct() <= 100
+    
+def test_send_mail():
+    mail_message = "Testnachricht"
+    mail_subject = "Testbetreff"
+    assert do_report(mail_message, mail_subject) == True
